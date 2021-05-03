6b52efe6c48da20f0855351ba0e6a797d7f68a54
Begemottt <55757703+Begemottt@users.noreply.github.com>
2021-05-03 14:59:54 -0700

    Initial commit!
    Not much here yet, still mostly working on learning React, which is a separate project, practically.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import '../scss/styles.scss';
+import html from '../index.html';
+import React from "react";
+import ReactDOM from "react-dom";
+import anime from 'node_modules/animejs/lib/anime.min.js';
+
